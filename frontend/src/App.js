d289caf5ae442d4cd9a8456601fb663ca4c74752
robsosa13 <rob.sosa13@gmail.com>
2022-07-21 20:44:06 -0400

    git init

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+const App = () => {
+
+  return(  <>  
+    <h1> Proyecto nuevo </h1>
+
+    </>)
+}
+// function App() {
+//   return (
+//     <>  
+//     <h1> Proyecto nuevo </h1>
+
+//     </>
+//   );
+// }
+
+export default App;
